3c37296ef5c27eb52df0c504041b8e243436cb87
Sunil Karki <karkisunil1200@gmail.com>
2020-02-21 11:12:21 -0600

    Update is working

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -7,12 +7,39 @@ const router = express.Router();
 router.get('/', (req, res) => {
   db.select('*')
     .from('accounts')
-    .then(account => {
-      res.status(200).json(account);
+    .then(accounts => {
+      res.status(200).json(accounts);
     })
     .catch(error => {
       console.log(error);
       res.status(500).json({message: 'Failed to get the list of accounts'});
+    });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  db('accounts')
+    .insert(body)
+    .then(account => {
+      res.status(200).json(body);
+    })
+    .catch(error => {
+      res.status(500).json({message: 'Failed to add an account'});
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const {id} = req.params;
+  const changes = req.body;
+
+  db('accounts')
+    .where({id})
+    .update(changes)
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({message: 'Cound not find the id'});
     });
 });
 
